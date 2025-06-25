8f1b2d4b118d96b894344755cf14696d9cd39fe6
teja569 <tejasaii1729@gmail.com>
2025-06-25 13:44:02 +0530

    reset

--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useSearchParams } from "react-router-dom";
 
 function ResetPassword() {
   const [searchParams] = useSearchParams();
